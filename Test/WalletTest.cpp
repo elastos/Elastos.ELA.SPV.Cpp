55d7189d463d36c7a4e105d856cbf81bae2fb8f1
ttblack <zhangxiaobin@elastos.org>
2018-04-12 17:40:20 +0800

    add TransactionOutputTest

--- Test/WalletTest.cpp
+++ Test/WalletTest.cpp
@@ -8,7 +8,34 @@
 
 using namespace Elastos::SDK;
 
-TEST_CASE( "Wallet test", "[Wallet]" )
+TEST_CASE( "Wallet listener test", "[Wallet]")
 {
+	class WalletListener : public Wallet::Listener {
+
+		// func balanceChanged(_ balance: UInt64)
+		virtual void balanceChanged(uint64_t balance) {
+			printf("balanceChanged");
+		}
+
+		// func txAdded(_ tx: BRTxRef)
+		virtual void onTxAdded(Transaction *transaction) {
+			printf("onTxAdded");
+		}
+
+		// func txUpdated(_ txHashes: [UInt256], blockHeight: UInt32, timestamp: UInt32)
+		virtual void onTxUpdated(const std::string &hash, uint32_t blockHeight, uint32_t timeStamp) {
+			printf("onTxUpdated");
+		}
+
+		// func txDeleted(_ txHash: UInt256, notifyUser: Bool, recommendRescan: Bool)
+		virtual void onTxDeleted(const std::string &hash, bool notifyUser, bool recommendRescan) {
+			printf("onTxDeleted");
+		}
+	};
+
+	SECTION("constructor width params") {
+		//todo completed me
+	}
+
 
 }
