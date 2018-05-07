5eac23ff15ff55924d7a472747be5f365146dc82
zhangchunlian <791398105@qq.com>
2018-05-06 21:20:29 -0700

    add PayloadSideMiningTest.cpp PayloadCoinBaseTest.cpp PayloadTransferCrossChainAssetTest.cpp Pay

--- SDK/ELACoreExt/Payload/PayloadCoinBase.cpp
+++ SDK/ELACoreExt/Payload/PayloadCoinBase.cpp
@@ -24,8 +24,25 @@ namespace Elastos {
 		}
 
 		ByteData PayloadCoinBase::getData() const {
-			return _coinBaseData;
+            return _coinBaseData;
+        }
+
+		void PayloadCoinBase::setCoinBaseData(const ByteData &coinBaseData) {
+		    uint8_t* buf = new uint8_t[coinBaseData.length];
+		    memcpy(buf, coinBaseData.data, coinBaseData.length);
+			_coinBaseData.data = buf;
+			_coinBaseData.length = coinBaseData.length;
 		}
+
+//		ByteData PayloadCoinBase::getData() const{
+//			ByteStream stream;
+//			Serialize(stream);
+//			uint8_t *buf = stream.getBuf();
+//			uint64_t len = stream.length();
+//
+//			return ByteData(buf, len);
+		    //return _coinBaseData;
+//		}
 
 		void PayloadCoinBase::Serialize(ByteStream &ostream) const {
 			ostream.putVarUint(_coinBaseData.length);
@@ -36,11 +53,21 @@ namespace Elastos {
 
 		void PayloadCoinBase::Deserialize(ByteStream &istream) {
 			uint64_t len = istream.getVarUint();
-			uint8_t *data = new uint8_t[len];
-			memset(data, 0, len);
-			istream.getBytes(data, len);
 
-			_coinBaseData = ByteData(data, len);
+			if (0 < len) {
+				uint8_t *data = new uint8_t[len];
+				if (data) {
+					memset(data, 0, len);
+					istream.getBytes(data, len);
+
+					if (_coinBaseData.data && data) {
+						delete[] _coinBaseData.data;
+						_coinBaseData.data = nullptr;
+					}
+
+					_coinBaseData = ByteData(data, len);
+				}
+			}
 		}
 	}
 }
