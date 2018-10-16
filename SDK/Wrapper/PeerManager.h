86ff52afbcea762242ee3079924d6f55858b8383
Yan Minzhi <yanmingzhi@elastos.org>
2018-10-16 12:29:37 +0800

    Implement messages initializing in peer class.

--- SDK/Wrapper/PeerManager.h
+++ SDK/Wrapper/PeerManager.h
@@ -178,7 +178,7 @@ namespace Elastos {
 
 			void fireSavePeers(bool replace, const std::vector<PeerInfo> &peers);
 
-			int fireNetworkIsReachable();
+			bool fireNetworkIsReachable();
 
 			void fireTxPublished(int error);
 
